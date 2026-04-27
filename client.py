257a90bbe4ac097d5167ecf6e06c550f06b5cf2b
mn <164825732+masaniii0909@users.noreply.github.com>
2026-04-27 11:34:10 -0400

    Implement websocket listener in client.py
    Add websocket listener to receive messages and check for wakeword.

--- client.py
+++ client.py
@@ -1,1 +1,25 @@
+import sys
+import os
+import random
+from word2number import w2n
+import subprocess as sp
+import websockets
+import asyncio
 
+wakeword = "computer"
+
+async def listen(uri):
+    async with websockets.connect(uri) as websocket:
+        while True:
+            print(await websocket.recv())
+  
+def main():
+    result = websocket.recv()
+    result = result[14:-3]
+    print(cleanresult)
+    if wakeword in cleanresult:
+        print("hi")
+
+if __name__ == "__main__":
+    asyncio.run(listen(f'ws://{ip}:{port}'))
+    main()
