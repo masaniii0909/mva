e1dfbcc25b480e4a3ebff5c1ab1ea6d58514cf9e
mn <164825732+masaniii0909@users.noreply.github.com>
2026-04-27 11:52:29 -0400

    Update websocket connection and message processing
    Refactor websocket message handling and add IP/port configuration.

--- client.py
+++ client.py
@@ -5,21 +5,24 @@ from word2number import w2n
 import subprocess as sp
 import websockets
 import asyncio
-
+ip = "127.0.0.1" # local
+port = 2700
 wakeword = "computer"
 
 async def listen(uri):
     async with websockets.connect(uri) as websocket:
         while True:
-            print(await websocket.recv())
-  
+            result = await websocket.recv()
+            result = result.split(':')[1]
+            print(result[2:-3])
 def main():
+    print('yea im here' * 5)
     result = websocket.recv()
     result = result[14:-3]
-    print(cleanresult)
+    print(result)
     if wakeword in cleanresult:
         print("hi")
 
-if __name__ == "__main__":
+while True:
     asyncio.run(listen(f'ws://{ip}:{port}'))
     main()
